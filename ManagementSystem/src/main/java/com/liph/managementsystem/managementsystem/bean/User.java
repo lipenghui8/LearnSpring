a6f7ae03b86e594cae92a48c41690ac7abf59a7b
lipenghuihenu <493284461@qq.com>
2022-04-16 20:03:49 +0800

    +1

---
+++ ManagementSystem/src/main/java/com/liph/managementsystem/managementsystem/bean/User.java
@@ -0,0 +1,13 @@
+package com.liph.managementsystem.managementsystem.bean;
+
+import lombok.Data;
+
+/**
+ * @author: Mr.Li
+ * @create: 2022-04-16 19:48
+ **/
+@Data
+public class User {
+    private String username;
+    private String password;
+}
