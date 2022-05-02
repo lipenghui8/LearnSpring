71560ed3852285011fff6b4a93df6bdc58bbbde3
lipenghuihenu <493284461@qq.com>
2022-05-02 16:38:54 +0800

    add

--- ManagementSystem/src/main/java/com/liph/managementsystem/managementsystem/bean/User.java
+++ ManagementSystem/src/main/java/com/liph/managementsystem/managementsystem/bean/User.java
@@ -1,12 +1,16 @@
 package com.liph.managementsystem.managementsystem.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author: Mr.Li
  * @create: 2022-04-16 19:48
  **/
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private String username;
     private String password;
--- ManagementSystem/src/main/java/com/liph/managementsystem/managementsystem/controller/TableController.java
+++ ManagementSystem/src/main/java/com/liph/managementsystem/managementsystem/controller/TableController.java
@@ -1,7 +1,12 @@
 package com.liph.managementsystem.managementsystem.controller;
 
+import com.liph.managementsystem.managementsystem.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author: Mr.Li
@@ -16,12 +21,23 @@ public class TableController {
     }
 
     @GetMapping("/dynamic_table")
-    public String dynamic_table(){
+    public String dynamic_table(Model model){
+        //表格内容的遍历
+        List<User> users = Arrays.asList(new User("zhangsan","123456"),
+                new User("lisi","123123"),
+                new User("wangwu","7890890"),
+                new User("hhh","fdsfsd"));
+        model.addAttribute("users",users);
         return "table/dynamic_table";
     }
 
     @GetMapping("/responsive_table")
     public String responsive_table(){
         return "table/responsive_table";
+    }
+
+    @GetMapping("/editable_table")
+    public String editable_table(){
+        return "table/editable_table";
     }
 }
