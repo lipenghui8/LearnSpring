1da98d4dc6e59c1e4c796e3d176c3a69fd5bdcc1
lipenghuihenu <493284461@qq.com>
2022-10-07 19:22:41 +0800

    +1

--- SpringBoot/Boot/src/main/java/com/liph/boot/bean/User.java
+++ SpringBoot/Boot/src/main/java/com/liph/boot/bean/User.java
@@ -1,11 +1,15 @@
 package com.liph.boot.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author: Mr.Li
  * @create: 2022-10-06 19:45
  **/
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class User {
     private String userName;
